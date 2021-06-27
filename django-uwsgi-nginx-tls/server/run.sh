2edaee155be31bddf022958c012426e0f22d7b40
ehrenb <branden.a.ehrenreich@gmail.com>
2021-06-27 15:34:39 -0700

    run as non-root

--- django-uwsgi-nginx-tls/server/run.sh
+++ django-uwsgi-nginx-tls/server/run.sh
@@ -3,6 +3,6 @@
 python3 manage.py collectstatic && \
 python3 manage.py makemigrations && \
 python3 manage.py migrate && \
-uwsgi --ini /root/mysite/uwsgi.ini &
+uwsgi --ini /app/mysite/uwsgi.ini &
 nginx -c /etc/nginx/nginx.conf
 #python3 manage.py runserver --noreload 0.0.0.0:8000
